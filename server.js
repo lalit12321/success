05ad4a98b2f09c45b2ed1326603292c73198e02e
lalit12321 <136174927+lalit12321@users.noreply.github.com>
2024-11-01 09:33:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const  send = require('process');
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.use(cors());
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('New client connected' + socket.id);
+
+    socket.on('send-location', (data) => {
+      //console.log('Received location:', data);
+        io.emit('receive-location', { id: socket.id, ...data });
+      });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        io.emit('user-disconnect', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+/*export default function handler(req, res) {
+  res.status(200).json({ message: 'Hello from Vercel!' });
+}*/
